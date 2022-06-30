7244b7d41fc175cad2cf581f244fc384680ce44b
Lautaro Villar <lautarovillar@outlook.com>
2022-06-30 16:36:40 -0300

    Segunda Entrega

---
+++ js/stock.js
@@ -0,0 +1,48 @@
+let stockProductos = [
+    {   id: 1, 
+        nombre: "Adidas Predator", 
+        cantidad: 1, 
+        precio: 12200, 
+        talle: 41, 
+        img: '../img/AD_FW7514-1.jpg'
+    },
+
+    {   id: 2, 
+        nombre: "Adidas Predator", 
+        cantidad: 1, 
+        precio: 15100, talle: 40, 
+        img: '../img/images.jpg'
+    },
+
+    {id: 3, 
+        nombre: "Adidas Copa", 
+        cantidad: 1, 
+        precio: 14200, 
+        talle: 44, 
+        img: '../img/BOTINES-ADIDAS-COPA-193-FG-HOMBRE_86321.webp'
+    },
+
+    {id: 4, 
+        nombre: "Adidas SpeedFlow", 
+        cantidad: 1, 
+        precio: 15400, 
+        talle: 45, 
+        img: '../img/FY3295-1.webp'},
+
+    {id: 5, 
+        nombre: "Adidas Edge", 
+        cantidad: 1, 
+        precio: 17200, 
+        talle: 42, 
+        img: '../img/AD_GV9876-1.jpg'
+    },
+
+    {id: 6, 
+        nombre: "Adidas Copa Sense", 
+        cantidad: 1, 
+        precio: 18500, 
+        talle: 42, 
+        img: '../img/617707-800-auto.webp'
+    },
+
+]
